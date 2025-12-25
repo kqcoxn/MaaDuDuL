80536998ccac242dfcbb6a6ad740c35d6084935d
kqcoxn <mjy03730@163.com>
2025-12-25 23:35:52 +0800

    feat: 领取通行证奖励

--- agent/customs/special_treat/receive_reward.py
+++ agent/customs/special_treat/receive_reward.py
@@ -31,3 +31,34 @@ class ReceiveTaskReward(CustomAction):
             return True
         except Exception as e:
             return Prompter.error("领取任务奖励", e)
+
+
+@AgentServer.custom_action("receive_pass_reward")
+class ReceivePassReward(CustomAction):
+    """"""
+
+    def run(self, context: Context, argv: CustomAction.RunArg) -> bool:
+        """"""
+        try:
+            # 解析参数
+            args = ParamAnalyzer(argv)
+            category = args.get(["category", "c"])
+
+            Prompter.log(f"领取{category}通行证奖励")
+            if category == "等级":
+                category = "grade_pass"
+            elif category == "便装":
+                category = "clothes_path"
+            elif category == "冒险":
+                category = "adv_pass"
+
+            context.run_task(
+                "领取奖励_领取通行证奖励开始",
+                {
+                    "领取奖励_进入通行证界面": {"template": f"main/{category}.png"},
+                },
+            )
+
+            return True
+        except Exception as e:
+            return Prompter.error("领取任务奖励", e)
